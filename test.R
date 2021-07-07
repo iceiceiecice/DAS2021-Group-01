c0480989b54047bd953c4a8d3eac88b164f52d41
iceiceiecice <xuzihe96@126.com>
2021-07-07 23:56:03 +0800

    test from local computer

---
+++ test.R
@@ -0,0 +1,22 @@
+## Test
+
+library(tidyverse)
+library(moderndive)
+library(skimr)
+library(kableExtra)
+library(gridExtra)
+library(broom)
+library(plotly)
+library(GGally)
+
+data <- read.csv("dataset1.csv")
+
+table(data$Type.of.Household)
+table(data$Region)
+
+data.score <- data %>%
+  dplyr::select(Total.Number.of.Family.members,Total.Household.Income,
+                Total.Food.Expenditure, Household.Head.Age, House.Floor.Area, House.Age)
+
+data.score %>%
+  cor()
